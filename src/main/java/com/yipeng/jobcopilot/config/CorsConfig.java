99fe59ae18e25cb35230f9c62d09fad7782325ed
Lazytodie11 <yipeng.li666@gmail.com>
2026-04-26 19:59:58 -0500

    Allow Vercel origins in CORS config

--- src/main/java/com/yipeng/jobcopilot/config/CorsConfig.java
+++ src/main/java/com/yipeng/jobcopilot/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "http://localhost:5174"
+                "http://localhost:5174",
+                "https://*.vercel.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
